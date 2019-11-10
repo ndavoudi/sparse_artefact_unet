e6b1c7a98b7dda2504ba83ed61c8d257e7f90c8f
ndavoudi <53782756+ndavoudi@users.noreply.github.com>
2019-11-10 18:04:30 +0100

    Update get_data.sh

--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -1,1 +1,11 @@
-to be uploaded...
+# All data for phanroms and experimental data could be found here: https://figshare.com/articles/data-mouse/9250634
+
+mkdir -p mice_ground_truth
+cd mice_ground_truth
+wget https://ndownloader.figshare.com/files/16842449
+
+
+cd ..
+mkdir -p mice_32_channels
+cd mice_32_channels
+wget https://ndownloader.figshare.com/files/16841759
